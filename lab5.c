371e064ed96768a91259f1429d0c9332913b9e9b
grythman <btuguldur397@gmail.com>
2024-02-16 17:45:37 +0800

    h

--- lab5.c
+++ lab5.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define ND 2 
+#define ND 2
 #define NS 2
-#define N (ND + NS) 
-void init(float x[], int n) {
-    for (int i = 0; i < n; i++) {
+#define N (ND + NS)
+void init(float x[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         x[i] = 0;
     }
 }
 
-int main() {
+int main()
+{
     int bas[NS];
-    float a[NS][N + 1] = { 
+    float a[NS][N + 1] = {
         {2, 1, 1, 0, 14}, // x1 + x2 + s1 = 14
         {4, 3, 0, 1, 28}  // 4x1 + 3x2 + s2 = 28
     };
@@ -26,57 +29,73 @@ int main() {
     for (i = 0; i < NS; i++)
         bas[i] = ND + i;
 
-    do {
+    do
+    {
         max = 0;
         kj = -1;
 
-        for (j = 0; j < N; j++) {
+        for (j = 0; j < N; j++)
+        {
             cj = -c[j];
-            for (i = 0; i < NS; i++) {
+            for (i = 0; i < NS; i++)
+            {
                 cj += a[i][j] * cb[i];
             }
-            if (cj > max) {
+            if (cj > max)
+            {
                 max = cj;
                 kj = j;
             }
         }
 
-        if (kj < 0) break;
+        if (kj < 0)
+            break;
 
         min = 1e30; // large number
         ki = -1;
-        for (i = 0; i < NS; i++) {
-            if (a[i][kj] < 1e-5) continue; 
+        for (i = 0; i < NS; i++)
+        {
+            if (a[i][kj] < 1e-5)
+                continue;
             t = a[i][N] / a[i][kj];
-            if (t < min) {
+            if (t < min)
+            {
                 min = t;
                 ki = i;
             }
         }
 
-        if (ki < 0) {
+        if (ki < 0)
+        {
             printf("Unbounded solution\n");
             return 1;
         }
 
         cb[ki] = c[kj];
         bas[ki] = kj;
-        
+
         t = a[ki][kj];
-        for (j = 0; j <= N; j++) a[ki][j] /= t;
-        for (i = 0; i < NS; i++) {
-            if (i != ki) {
+        for (j = 0; j <= N; j++)
+            a[ki][j] /= t;
+        for (i = 0; i < NS; i++)
+        {
+            if (i != ki)
+            {
                 t = a[i][kj];
-                for (j = 0; j <= N; j++) a[i][j] -= t * a[ki][j];
+                for (j = 0; j <= N; j++)
+                    a[i][j] -= t * a[ki][j];
             }
         }
     } while (1);
 
     z = 0;
-    for (i = 0; i < NS; i++) {
-        if (bas[i] < ND) x[bas[i]] = a[i][N];
+    for (i = 0; i < NS; i++)
+    {
+        if (bas[i] < ND)
+            x[bas[i]] = a[i][N];
     }
-    for (i = 0; i < ND; i++) {
+    for (i = 0; i < ND; i++)
+    {
         z += x[i] * -c[i];
     }
 
